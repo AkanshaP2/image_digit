bb59152e4155a5191c9d0197358bbcfcfcbb5da3
Akansha Pandey <127675854+AkanshaP2@users.noreply.github.com>
2024-07-01 14:12:58 +0530

    Add files via upload

---
+++ dataimage.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+mkdir -p data
+cd data
+
+# Download files from GitHub
+wget https://github.com/AkanshaP2/data_files/raw/main/1.png
+wget https://github.com/AkanshaP2/data_files/raw/main/2.png
+wget https://github.com/AkanshaP2/data_files/raw/main/3.png
+wget https://github.com/AkanshaP2/data_files/raw/main/4.png
+wget https://github.com/AkanshaP2/data_files/raw/main/5.png
+
+
+
