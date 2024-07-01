e7df79cd9fc62857d9815b3f462b97d7c8e1f346
Akansha Pandey <127675854+AkanshaP2@users.noreply.github.com>
2024-07-01 14:13:40 +0530

    Add files via upload

---
+++ imagedigit.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir -p data
+cd data
+
+# Download files from GitHub
+wget https://github.com/AkanshaP2/image_digit/raw/main/image1.png
+wget https://github.com/AkanshaP2/image_digit/raw/main/image2.png
+wget https://github.com/AkanshaP2/image_digit/raw/main/image3.png
+
+
